0f123b1de6136069a734ddceca64b4da2591b5ee
IgorHelinski <igor.www.pl.com@gmail.com>
2022-12-11 01:12:11 +0100

    a guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+#![allow(unused)]
+use std::cmp::Ordering;
+use std::io;
+use std::sync::BarrierWaitResult;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    //1 generate the secret number
+    let secret: i32 = rand::thread_rng().gen_range(1..101);
+
+    //2 ask user number
+    println!("Guess the number: (hint {}):", secret);
+
+    loop {
+        //3 read user number as a string
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        //4 verify user number and tell user
+        let guess: i32 = guess.trim().parse().expect("failed, plese type a number");
+        println!("Your guess: {}", guess);
+        match guess.cmp(&secret){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal =>
+            {
+            println!("You win!!");
+            break;
+            } 
+        }  
+    }
+    
+    println!("\nGoodbye\n");
+    
 }
+
+
